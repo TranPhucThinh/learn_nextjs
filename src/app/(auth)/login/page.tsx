47b9856fda55f0a4893b83a532c4817220a31ec5
thinhtp <thinhtp@greenglobal.vn>
2025-01-12 21:19:43 +0700

    Feat: register page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold text-center">Đăng nhập</h1>
+    </div>
+  )
+}
+
+export default LoginPage
